44ef6aac112eb26b5f213542b8577e70b356b64c
Prasanna Vijayan <prasannavijayan446@gmail.com>
2018-12-29 09:22:41 +0530

    Adds little of about me

--- app/components/sect-ion.js
+++ app/components/sect-ion.js
@@ -3,7 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
 	
-  classNames  : [ 'd-flex section bg-black' ],
+  classNames  : [ 'd-flex section bg-grey' ],
   userData    : service()
 
 });
